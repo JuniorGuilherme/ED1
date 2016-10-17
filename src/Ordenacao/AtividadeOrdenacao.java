d533c9f749a6afd071fa1c3b2828aec1fe033faa
Junior Guilherme <Foco18@#>
2016-10-16 23:12:58 -0200

    Desenvolvimento Ordenacao

--- src/Ordenacao/AtividadeOrdenacao.java
+++ src/Ordenacao/AtividadeOrdenacao.java
@@ -6,16 +6,37 @@ import java.util.Random;
  * Created by Junior Guilherme on 16/10/2016.
  */
 public class AtividadeOrdenacao {
+    public static void mostrar(int[] x){
+        System.out.println("Ordenacao:");
+        for(int i=0; i<x.length; i++){
+            System.out.println(x[i]);
+        }
+    }
     public static void main(String[] args) {
-        int vetor1[] = new int[20], vetor2[], vetor3[];
+        int vetor1[] = new int[100000], vetor2[] = new int[100000], vetor3[] = new int[100000];
+        long elapsed1, elapsed2, elapsed3;
         Random gerador = new Random();
         MetodosSorts ms = new MetodosSorts();
-
+        //Loop para atribuicao dos numeros randomicos aos vetores.
         for(int i =0; i<vetor1.length; i++){
-            vetor1[i]=gerador.nextInt(10000);
+            vetor1[i]=gerador.nextInt(100000);
             System.out.println(vetor1[i]);
+            vetor2[i]=vetor1[i];
+            vetor3[i]=vetor1[i];
         }
-        vetor2=vetor1;
-        vetor3=vetor1;
+        //Chamada dos metodos de ordenacao com retorno em Milisegundos para as variaveis longs.
+        elapsed3 = ms.bubleSort(vetor3);
+        elapsed1 = ms.selectSort(vetor1);
+        elapsed2 = ms.insertSort(vetor2);
+        //Metodo mostrar apenas para verificacao de fidelidade dos metodos de ordenacao.
+        //Nao testar com muitos elementos nos vetores.
+        mostrar(vetor1);
+        mostrar(vetor2);
+        mostrar(vetor3);
+        //Exibicao dos times dos metodos de ordenacao.
+        System.out.println("Tempos de Execucao");
+        System.out.println("Selection Sort: "+elapsed1+" ms");
+        System.out.println("Insertion Sort: "+elapsed2+" ms");
+        System.out.println("Bublle Sort: "+elapsed3+" ms");
     }
 }
--- src/Ordenacao/MetodosSorts.java
+++ src/Ordenacao/MetodosSorts.java
@@ -4,4 +4,57 @@ package Ordenacao;
  * Created by Junior Guilherme on 16/10/2016.
  */
 public class MetodosSorts {
+    public long selectSort(int[] x){
+        long start = System.currentTimeMillis();
+        int aux;
+        for (int f = 0; f < x.length; f++)
+        {
+            aux = x[f];
+            for (int g = f; g < x.length; g++)
+            {
+                if (x[g] < aux)
+                {
+                    aux = x[g];
+                    x[g] = x[f];
+                    x[f] = aux;
+                }
+            }
+        }
+        return System.currentTimeMillis() - start;
+    }
+
+    public long insertSort(int[] x){
+        long start = System.currentTimeMillis();
+        int aux;
+        for(int i=1; i<x.length; i++){
+            for(int f=0; f<i; f++){
+                if(x[i]<x[f]){
+                    aux=x[f];
+                    x[f]=x[i];
+                    x[i]=aux;
+                }
+            }
+        }
+        return System.currentTimeMillis() - start;
+    }
+    public long bubleSort(int[] x){
+        long start = System.currentTimeMillis();
+        int aux;
+        boolean ord;
+            for (int j = 1; j < x.length; j++) {
+                ord = true;
+                for (int i = 1; i < x.length; i++) {
+                    if (x[i - 1] > x[i]) {
+                        aux = x[i - 1];
+                        x[i - 1] = x[i];
+                        x[i] = aux;
+                        ord = false;
+                    }
+                }
+                if(ord){
+                    break;
+                }
+            }
+        return System.currentTimeMillis() - start;
+    }
 }
