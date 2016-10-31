9cfda7f0b4440eecfc91fd457df2dab3f362e85a
Junior Guilherme <Foco18@#>
2016-10-31 15:18:21 -0200

    Finalização do algoritmo de lista encadeada.

--- src/ListaEncadeada/Principal.java
+++ src/ListaEncadeada/Principal.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 /**
  * Created by jrg_c on 18/10/2016.
+ * Questões pertinentes:
+ * 1- Quais tipos de listas encadeadas existem?
+ * R: Listas encadeadas simples, duplamente encadeadas e encadeadas circulares.
+ * 2- Onde é ou pode ser implementado uma lista encadeada?
+ * R: Geralmente implementado em soluções de baixo nivel para controle de hardware, como em sistemas embarcados.
  */
 public class Principal {
     public static void main(String[] args) {
